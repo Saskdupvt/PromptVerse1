1e5d92eb5f6205e0b57e6564304e7f485fc51d7b
Bitgodi <piyumika40@gmail.com>
2026-01-18 00:32:32 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,8 @@
 // Supabase CDN එක හරහා සම්බන්ධ වීම
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'omflycicfddvrizdjzcf'
+// ඔබගේ Project ID එක URL එකක් ලෙස මෙසේ සැකසිය යුතුය
+const supabaseUrl = 'https://omflycicfddvrizdjzcf.supabase.co' 
 const supabaseAnonKey = 'sb_publishable_l-rATJ_SjrkQS-R-BamB1g_cXtPYoCl'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
